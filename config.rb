7e5444e068e06b49ae81a817cbd9c22058ad89e0
FearGoidte <FearGoidte@users.noreply.github.com>
2016-01-21 10:32:39 +0000

    Switched to X.H.T.M.L.

--- config.rb
+++ config.rb
@@ -3,68 +3,40 @@
 ###
 
 # Per-page layout changes:
-#
-# With no layout
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
-
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+page "/*.xhtml", layout: 'layout'
 
 ###
 # Helpers
 ###
 
+# Blog
 activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
-
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
-  # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
-
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
-
+  blog.permalink = "{year}/{month}/{day}/{title}.xhtml"
+  blog.sources = "{year}-{month}-{day}-{title}.xhtml"
+  blog.taglink = "tags/{tag}.xhtml"
+  blog.layout = "layout"
+  blog.year_link = "{year}.xhtml"
+  blog.month_link = "{year}/{month}.xhtml"
+  blog.day_link = "{year}/{month}/{day}.xhtml"
+  blog.tag_template = "tag.xhtml"
+  blog.calendar_template = "calendar.xhtml"
   # Enable pagination
   # blog.paginate = true
   # blog.per_page = 10
   # blog.page_link = "page/{num}"
 end
 
-page "/feed.xml", layout: false
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+# Activate directory indices and remove file extensions from urls
+activate :directory_indexes
+set :index_file, 'index.xhtml'
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+# Markdown settings
+set :markdown, :xhtml => true
+set :markdown_engine, :redcarpet
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
   # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
 end
 
 # Deploy to GitHub
