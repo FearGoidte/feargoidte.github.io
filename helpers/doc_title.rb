28186aba3631018f589d4e5a629e358670e51dcc
FearGoidte <FearGoidte@users.noreply.github.com>
2016-06-06 14:40:51 +0100

    Renamed variables

--- helpers/doc_title.rb
+++ helpers/doc_title.rb
@@ -1,14 +1,14 @@
 def doc_title
   seperator = '—'
-  site_title = data.settings.site.name
+  site_name = data.settings.site.name
   if @title
     page_title = @title
   elsif current_resource.data.title
     page_title = current_resource.data.title
   end
   if page_title
-    page_title << seperator << site_title
+    page_title << seperator << site_name
   else
-    return site_title
+    return site_name
   end
 end
