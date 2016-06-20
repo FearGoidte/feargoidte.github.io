b877209ee50e09f8d378e93a437dd9ecbc8e8f94
FearGoidte <FearGoidte@users.noreply.github.com>
2016-06-20 10:03:53 +0100

    R.D.F.a. amendments
    Added Website type and moved copyrightYear to span to conform to format.

--- helpers/prettify_date_time.rb
+++ helpers/prettify_date_time.rb
@@ -5,10 +5,25 @@ def prettify_date(date)
   #{date.strftime('%B %Y')}"
 end
 
+def prettify_date_copyright(date)
+  number = date.day.ordinalize
+  ordinal = number.slice!(-2, 2)
+  "#{date.strftime('%A')}, #{number}<sup>#{ordinal}</sup>
+  #{date.strftime('%B')} <span property='schema:copyrightYear'>#{date.strftime("%Y")}</span>"
+end
+
 def prettify_date_time(date_time)
   number = date_time.day.ordinalize
   ordinal = number.slice!(-2, 2)
   "#{date_time.strftime('%A')}, #{number}<sup>#{ordinal}</sup>
   #{date_time.strftime('%B %Y')} at
+  #{date_time.strftime('%H:%M')} (#{Time.zone.name})"
+end
+
+def prettify_date_time_copyright(date_time)
+  number = date_time.day.ordinalize
+  ordinal = number.slice!(-2, 2)
+  "#{date_time.strftime('%A')}, #{number}<sup>#{ordinal}</sup>
+  #{date_time.strftime('%B')} #{date_time.strftime('%Y')} at
   #{date_time.strftime('%H:%M')} (#{Time.zone.name})"
 end
